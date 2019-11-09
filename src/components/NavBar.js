3c2d2773b2727aa5ceca004029e46256de2d1a57
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2019-11-10 01:50:44 +0200

    Include drawers to the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,104 @@
 import React from "react";
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import HomeIcon from "@material-ui/icons/Home";
-import Typography from "@material-ui/core/Typography";
+import Person from "@material-ui/icons/Person";
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+}));
 
 function NavBar() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({...state, [side]: open});
+    };
+
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(side, false)}
+            onKeyDown={toggleDrawer(side, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                        <ListItemText primary={text}/>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                        <ListItemText primary={text}/>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
     return (
-        <AppBar color="primary" position="static">
-            <Toolbar>
-                <HomeIcon fontSize="small" style={{ "marginBottom": "0.4em"}}/>
-                <Typography gutterBottom variant="h6">
-                    | Working Hours <strong>3.0</strong>
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit" aria-label="menu"
+                        onClick={toggleDrawer('left', true)}>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+                        {sideList('left')}
+                    </Drawer>
+                    <Typography variant="h6" className={classes.title}>
+                        Working Hours 3.0
+                    </Typography>
+                    <Button color="inherit"><Person/></Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
 
