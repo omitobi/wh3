7d629c2c2a3a2c0a5309f936fe4e5437a3d24784
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2019-11-17 10:57:37 +0200

    save time elapsed as object

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -3,7 +3,11 @@ import moment from 'moment';
 
 export default function CountDown() {
     const [timer, setTimer] = useState({
-        timeElapsed: "0:00:00",
+        timeElapsed: {
+            hour: "0",
+            minute: "00",
+            seconds: "00"
+        },
         totalSeconds: 0,
         started: false,
     });
@@ -18,30 +22,50 @@ export default function CountDown() {
         setTimer({
             totalSeconds: totalSeconds,
             timeElapsed: timer.timeElapsed,
-            started: timer.started,
+            started: true,
         });
 
-        let hour = Math.floor(totalSeconds / 3600);
+        let hour = getHour(totalSeconds);
+        let minute = getMinute(totalSeconds, hour);
+        let seconds = getSeconds(totalSeconds, hour, minute);
+
+        setTimer({
+            timeElapsed: {hour, minute, seconds},
+            totalSeconds: totalSeconds,
+            started: true,
+        });
+    };
+
+    const getHour = (totalSeconds) => {
+        return Math.floor(totalSeconds / 3600);
+    };
+
+    const getMinute = (totalSeconds, hour) => {
         let minute = Math.floor((totalSeconds - hour * 3600) / 60);
         if (minute < 10) {
             minute = "0" + minute;
         }
+
+        return minute;
+    };
+
+    const getSeconds = (totalSeconds, hour, minute) => {
         let seconds = totalSeconds - (hour * 3600 + minute * 60);
         if (seconds < 10) {
             seconds = "0" + seconds;
         }
-        const timeElapsed = hour + ":" + minute + ":" + seconds;
-        setTimer({
-            timeElapsed,
-            totalSeconds: totalSeconds,
-            started: timer.started,
-        });
+
+        return seconds;
+    };
+
+    const getFullTimeString = () => {
+        return timer.timeElapsed.hour + ":" + timer.timeElapsed.minute + ":" + timer.timeElapsed.seconds;
     };
 
     return (
         <div>
             <input type="button" onClick={getTime} disabled={timer.started} value="Start timer"/>
-            <em>{timer.timeElapsed}</em>
+            <em>{getFullTimeString()}</em>
         </div>
     );
 
